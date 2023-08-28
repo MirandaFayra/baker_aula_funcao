83cdf57212b32b27d7d79ea72c3c655609f80dcc
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-08-28 20:28:28 -0300

    adicionar a base dos exs

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 console.log(`Salveee`)
+
+// let => muda | const => não muda
+// ===  igual e do mesmo tipo 
+// !== diferente 
+// n1 > n2  número 1 é maior que o 2
+// n1 < n2  número 1 é menor que o 2
+
+const numero1 = 10
+const numero2 = 15
+
+
+// 1. numero 1 igual ao 2 
+let comparacaoEntreNumeros = numero1 === numero2
+console.log(`Os números são iguais? ${comparacaoEntreNumeros}`)
+
+// 2. numero 1 diferente do 2
+let diferencaEntreNumeros = numero1 !== numero2
+console.log(`Os números são diferentes? ${diferencaEntreNumeros}`)
+
+// 2. numero 1 maior que o 2
+let primeiroMaiorQueSegundo = n1 > n2
+console.log(`O primeiro número é maior que o segundo? ${primeiroMaiorQueSegundo}`)
+
+// 2. numero 1 menor que 2
+let primeiroMenorQueSegundo = n1 < n2
+console.log(`O primeiro número é menor que o segundo ? ${primeiroMenorQueSegundo} `);
